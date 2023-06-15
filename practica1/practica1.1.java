b68d322dd34e11b2994734b0d35eb4aedd156428
emx200 <136612021+emx200@users.noreply.github.com>
2023-06-14 22:08:24 -0600

    Create practica1.1.java

---
+++ practica1/practica1.1.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author ema
+ */
+public class PRACT_1_1 {
+    public static void main(String[] args) {
+        
+        //asigno la variable a 10 
+        String num="10";
+        
+        //asigno el salario
+        float salario=780993.67f;
+        
+        //asigno el peso 
+        double peso=68.9;
+        
+        //asigno la estatura 
+        double estatura=1.67;
+        
+        //asigno la edad
+        String edad="12";
+        
+        //asigno el año
+        short añoNacimineto=1990;
+        
+        //asigno que es verdadero que es estudiante 
+        boolean isEstudiante=true;
+        
+        int convNum=Integer.parseInt(num);
+        
+       String  convSalario=String.valueOf(salario);
+       
+       float convPeso=(float)peso;
+       
+       String convEstatura=String.valueOf(estatura);
+       
+       int convEdad=Integer.parseInt(edad);
+       
+       String convAnoNac=(String)"añoNacimiento";
+       
+       String ConvIsNac=String.valueOf(isEstudiante);
+        
+       
+        
+        
+        
+        
+        
+        
+    }
+    
+}
