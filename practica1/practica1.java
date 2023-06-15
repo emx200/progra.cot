96f58682b9556a168531cb0814e671703e8dc984
emx200 <136612021+emx200@users.noreply.github.com>
2023-06-14 22:05:45 -0600

    Create practica1.java

---
+++ practica1/practica1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author Josué
+ */
+public class PRACT_1 {
+      //Definición de contantes 
+      static  final double PI = 3.1416;
+      static final boolean ACTIVO=false;
+      static final double IVA=0.13;
+    public static void main(String[] args) {
+        
+        //Definición de variables 
+        int fDia=8;
+        String nCedula;
+        float salario;
+        char genero;
+        int edad;
+        boolean esFin=true;
+        String nombre;
+        boolean esCierto;
+        int num1=2;
+        
+      
+      
+      //inizialización de variables 
+      salario= 51564;
+      genero='F';
+      nCedula="605780943";
+      edad=58;
+      esFin=false;
+      num1 ++;
+      
+      
+      
+      
+       
+        
+    }
+    
+}
