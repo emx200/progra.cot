7461f72e6882282c265f3eff80f4da1d47319795
emx200 <136612021+emx200@users.noreply.github.com>
2023-06-14 22:14:15 -0600

    Add files via upload

---
+++ Operadores1.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ema
+ */
+public class Operadores1 {
+    public static void main(String[] args) {
+        
+        //declaración de varibales 
+        String nom="emanuel ";
+        String ap1="aguilar ";
+        String ap2="campos ";
+        
+        // suma de variables para creación del nombre completo 
+        String nomcomp=nom+ap1+ap2;
+        
+        //muesta del nombre completo 
+        System.out.println("El nombre completo es "+nomcomp);
+        
+        //declaración de variables 
+        String a="el";
+        String b="mundo ";
+        String c="esta";
+        String d="de cabeza";
+        
+        //suma de variables para la creación de la frase
+        String Frase=a+b+c+d;
+        
+        //muesta de la frase 
+        System.out.println("La frase es "+Frase);
+        
+        //declaración de variables 
+        int n1=120;
+        int n2=34;
+        int n3=56;
+        int n4=5;
+        
+        //suma de numeros 
+        int sum=n1+n2+n3+n4;
+         System.out.println("El resultado de la suma es "+sum);
+         
+         //resta de numeros
+        int rest=n1-n2-n3-n4;
+        System.out.println("El resultado de la resta es "+rest);
+        
+        //multiplicación de números
+        int mult=n1*n2*n3*n4;
+        System.out.println("El resultado de la multiplicación "+mult);
+        
+        //división de números
+        int divi=n1/n2/n3/n4;
+        System.out.println("El resultado de la división es "+divi);
+        
+        //residuo de la división 
+        int resdivi=n1%n2%n3%n4;
+        System.out.println("El residuo de la division es "+resdivi);
+  
+        //incremento de la variable 
+        n1++;
+        
+        //decremento de la variable 
+        n4++;
+        
+        //declaración de variables 
+        boolean condicion=true;
+        boolean t=!condicion;
+        
+        //muestra de las variables 
+        System.out.println(condicion);
+        System.out.println(t);
+        
+        //declaración de variables 
+        int h;
+        int f;
+        int m;
+        int r;
+        
+        //asignación de un valor a las variables 
+        h=5;
+        f=8;
+        m=3;
+        r=4;
+        
+        //suma y muestra de la variable 
+        h+=6;
+        System.out.println("|la suma es "+h);
+        
+        //resta y muestra de la variable
+        f-=2;
+        System.out.println("la resta es "+f);
+        
+        //multiplicación y muestra de la variable 
+        m*=2;
+        System.out.println("la multiplicación es "+m);
+        
+        //división y muestra de la variable 
+        r/=1;
+        System.out.println("la división es "+r);
+        
+        
+        
+        
+        
+        
+        
+    }
+}
