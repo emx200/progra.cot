aa3a5cba43663fe1b0923ab4a0b04f7f0e6810b2
emx200 <136612021+emx200@users.noreply.github.com>
2023-06-14 22:42:54 -0600

    Add files via upload

---
+++ practica2/operadores2.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Emanuel
+ */
+public class operadores2 {
+    
+    public static void main(String[] args) {
+            //primera frase
+            String nom = "Emanuel ";
+            
+            String ap1 = "Aguiar ";
+            
+            String ap2 = "Campos ";
+            
+            String nombreCompleto = nom+ap1+ap2;
+            
+            System.out.println("El nombre  completo es "+ nombreCompleto);
+            
+            
+            //segunda frase
+            String a = "el ";
+            
+            String b = "mundo ";
+            
+            String c = "esta ";
+            
+            String d = "de cabeza ";
+            
+            String fraseCompleta = a+b+c+d;
+            
+            System.out.println("La frase es " + fraseCompleta);
+            
+            
+            //operaciones
+            int n1 = 120;
+            
+            int n2 = 34;
+            
+            int n3 = 56;
+            
+            int n4 = 5;
+            
+            //suma
+            int sumaTotal = n1+n2+n3+n4;
+            
+            System.out.println("El resultado es " + sumaTotal);
+            
+            //resta
+            int restaTotal = n1-n2-n3-n4;
+            
+            System.out.println("El resultado es " + restaTotal);
+            
+            //multipicacion
+            int multiTotal = n1*n2*n3*n4;
+            
+            System.out.println("El resutado es " + multiTotal);
+            
+            //division
+            int diviTotal = n1/n2/n3/n4;
+            
+            System.out.println("El resultado es " + diviTotal);
+            
+            //modulo
+            int reciTotal = n1%n2%n3%n4;
+            
+            System.out.println("El resultado es " + reciTotal);
+            
+            //suma
+            n1++;
+            
+            n4++;
+            
+            //variable
+            boolean condicion = true;
+            boolean t = false;
+        
+            //variable
+            int h = 5;
+            
+            int f = 8;
+            
+            int m = 3;
+            
+            int r = 4;
+            
+            
+            //suma
+            int suma = h+6;
+            
+            System.out.println("El resultado es " + suma);
+            
+            //resta
+            int resta = f-2;
+            
+            System.out.println("El resultado es " + resta);
+            
+            //multiplicacion
+            int multiplicacion = m*2;
+            
+            System.out.println("El resultado es " + multiplicacion);
+            
+            //division
+            int division = r/1;
+            
+            System.out.println("El resultado es " + division);
+            
+            
+    }
+    
+}
